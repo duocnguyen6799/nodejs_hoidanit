634342fcff0aad207ee57d76bab3a273020f044e
DuocNP <duocnguyen6799@gmail.com>
2022-11-20 22:35:22 +0700

    connect to mysql db

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,30 @@
+import connection from '../configs/connectDB'
+
 
 let getHomepage = (req, res) => {
-    return res.render('index.ejs');
+
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            console.log('>>>> mysql check');
+            console.log(results); // results contains rows returned by server
+
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            })
+            return res.render('index.ejs', {
+                dataUser: JSON.stringify(data)
+            })
+        }
+    )
+    return res.render('index.ejs')
 };
 
 let getAboutPage = (req, res) => {
